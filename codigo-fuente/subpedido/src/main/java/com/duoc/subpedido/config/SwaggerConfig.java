bdcd4b023163dc53ea12af6658aa30122f17b879
Misael Concha <mis.concha@duocuc.cl>
2026-06-01 17:20:25 +0000

    agregado realizados de swagger

---
+++ codigo-fuente/subpedido/src/main/java/com/duoc/subpedido/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.duoc.subpedido.SwaggerConfig;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration       // Le dice a Spring que esta clase tiene configuraciones
+public class SwaggerConfig {
+
+    @Bean            // Spring ejecuta este método al iniciar y registra el resultado
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("API Subpedidos Service")          // ← cambia por TU servicio
+                .version("1.0")
+                .description("Gestión de Subpedidos, y añadir pedidos al mismo."));
+    }
+}
--- codigo-fuente/subpedido/src/main/java/com/duoc/subpedido/controller/SubpedidoController.java
+++ codigo-fuente/subpedido/src/main/java/com/duoc/subpedido/controller/SubpedidoController.java
@@ -15,8 +15,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+// Imports de Swagger (agregar estos)
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.List;
 
+@Tag(name= "Subpedidos", description = "Operaciones para añadir un Subpedido a nuestro sistema")
 @RestController
 @RequestMapping("/api/v1/subpedidos")
 public class SubpedidoController {
@@ -25,12 +33,20 @@ public class SubpedidoController {
     private SubpedidoService service;
 
     // LISTAR TODOS
+    @Operation(summary = "Listar todas los subpedidos",
+               description = "Retorna la lista completa de subpedidos registradas en el sistema.")
+    @ApiResponse(responseCode = "200", description = "Lista obtenida exitosamente")
     @GetMapping
     public List<Subpedido> listarTodos() {
         return service.listarTodos();
     }
 
     // OBTENER POR ID
+    @Operation(summary = "Buscar Subpedidos por ID")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Subpedido encontrada"),
+        @ApiResponse(responseCode = "404", description = "Subpedido no encontrada")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<SubpedidoDTO> obtenerPorId(
             @PathVariable Long id) {
@@ -43,6 +59,8 @@ public class SubpedidoController {
     }
 
     // CREAR SUBPEDIDO
+    @Operation(summary = "Registrar nuevo Subpedido con endopoint /generar")
+    @ApiResponse(responseCode = "201", description = "Subpedido creado exitosamente")
     @PostMapping("/generar")
     public ResponseEntity<SubpedidoDTO> crearSubpedido(
             @Valid @RequestBody SubpedidoCreateDTO dto) {
@@ -54,6 +72,11 @@ public class SubpedidoController {
     }
 
     // LISTAR POR STAND
+    @Operation(summary = "Buscar Subpedidos por Stand")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Subpedido encontrada"),
+        @ApiResponse(responseCode = "404", description = "Subpedido no encontrada")
+    })
     @GetMapping("/stand/{standId}")
     public List<Subpedido> listarPorStand(
             @PathVariable Long standId) {
@@ -62,6 +85,11 @@ public class SubpedidoController {
     }
 
     // LISTAR POR PEDIDO
+    @Operation(summary = "Buscar Subpedidos por IDSubpedido")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Subpedido encontrada"),
+        @ApiResponse(responseCode = "404", description = "Subpedido no encontrada")
+    })
     @GetMapping("/pedido/{pedidoId}")
     public List<Subpedido> listarPorPedido(
             @PathVariable Long pedidoId) {
@@ -70,6 +98,11 @@ public class SubpedidoController {
     }
 
     // ASIGNAR STAND
+    @Operation(summary = "Actualizar Stand existente con endpoint /{id}/asignarStand/{standId}")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Actualización exitosa"),
+        @ApiResponse(responseCode = "404", description = "Subpedido no encontrada")
+    })
     @PutMapping("/{id}/asignarStand/{standId}")
     public ResponseEntity<Subpedido> asignarStand(
             @PathVariable Long id,
@@ -84,6 +117,11 @@ public class SubpedidoController {
     }
 
     // ACTUALIZAR ESTADO
+    @Operation(summary = "Actualizar Estado existente con endpoint /{id}/estado")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Actualización exitosa"),
+        @ApiResponse(responseCode = "404", description = "Subpedido no encontrada")
+    })
     @PutMapping("/{id}/estado")
     public ResponseEntity<Subpedido> actualizarEstado(
             @PathVariable Long id,
@@ -99,6 +137,11 @@ public class SubpedidoController {
     }
 
     // PROBAR MICROSERVICIO STAND
+    @Operation(summary = "Buscar Stand en otro microservicio")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Stand encontrada"),
+        @ApiResponse(responseCode = "404", description = "Stand no encontrada")
+    })
     @GetMapping("/stand/externo")
     public StandDTO probarStand(
             @PathVariable Long id) {
@@ -107,10 +150,27 @@ public class SubpedidoController {
     }
 
     // PROBAR MICROSERVICIO PEDIDO
+    @Operation(summary = "Buscar Pedido en otro microservicio")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Pedido encontrada"),
+        @ApiResponse(responseCode = "404", description = "Pedido no encontrada")
+    })
     @GetMapping("/pedido/externo")
     public PpordenDTO probarPporden(
             @PathVariable Long id) {
 
         return service.obtenerPporden(id);
     }
+
+    /*
+    @Operation(summary = "Eliminar Subpedido")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Eliminación exitosa"),
+        @ApiResponse(responseCode = "404", description = "Subpedido no encontrada")
+    })
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> eliminar(
+            @Parameter(description = "ID del Subpedido a eliminar")
+            @PathVariable Long id) {
+    */
 }
